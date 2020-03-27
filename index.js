8d7a55e33ac7a67c9670894b96f37bbe3018c61c
Victor Fernandez <vmfs99@gmail.com>
2020-03-26 20:25:43 -0400

    done

--- index.js
+++ index.js
@@ -1,8 +1,64 @@
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 
-let server = http.createServer((req,res)=>{
-    res.end("Hello from the server")
+const slugify = require('slugify')
+
+const replaceTemplate = require('./modules/replaceTemplate')
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productData = JSON.parse(data);
+
+const slugs = productData.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs)
+
+const server = http.createServer((req,res)=>{
+    const {query, pathname} = url.parse(req.url, true);
+
+    // Overview page
+    if (pathname === '/' || pathname === '/overview') {
+
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+
+        const cardsHtml = productData.map(el => replaceTemplate(tempCard, el)).join('')
+        console.log(cardsHtml)
+
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+
+        res.end(output)
+    }
+    
+    // Product page
+    else if (pathname === '/product') {
+        const product = productData[query.id]
+        const output = replaceTemplate(tempProduct, product)
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        res.end(output)
+        
+    }
+    
+    // API
+    else if (pathname === '/api') {
+        res.writeHead(200, {'Content-type': 'application/json'})
+        res.end(data)
+    }
+    
+    // Not found
+    else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        })
+        res.end('<h1>Page not found!</h1><br><a href="/">Go back home</a>')
+    }
 })
 
 server.listen(8000, '127.0.0.1', ()=>{
